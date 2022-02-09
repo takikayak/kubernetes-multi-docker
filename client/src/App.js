d29ec764c59fd74c610d5f19c22c33fe01e6c1e5
Takacs Zsolt <takacs.zsolt@nisz.hu>
2022-02-09 18:08:38 +0100

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>AWS</p>
           <a
             className="App-link"
             href="https://reactjs.org"
